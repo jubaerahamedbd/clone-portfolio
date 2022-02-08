14bdb2e40dbf5d3be6e96ce8702deb47da35a1a2
Jubaer Ahamed <jahamed171200@bscse.uiu.ac.bd>
2022-02-09 01:07:42 +0600

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { Section } from '../../styles/GlobalComponents';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+8801947425488">01947425488</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:jubaerahamed@gmail.com">jubaerahamed@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovation one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href='https://github.com/jubaerahamedbd'>
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href='https://www.linkedin.com/in/jubaerahamedbd/'>
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href='https://facebook.com/jubaerahamedbd'>
+            <AiFillFacebook size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
